4259e48c20ba50a355a3968bc6bde12242ebe3f7
GreenMonkeySaveEarth <changyunglin@gmail.com>
2025-07-20 13:32:43 -0700

    Implement drinks routes with rate limiting and authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,33 @@
 import { Router } from "express";
 import * as controller from "../controllers/index";
+import { validateCreateDrink } from "../middlewares/validation";
+import { searchRateLimit, writeRateLimit } from "../middlewares/rateLimit";
+import { AuthMiddleware } from "../middlewares/auth";
+import { AuthorizationMiddleware } from "../middlewares/authorization";
 
 export const index = Router();
 
 index.get("/", controller.index);
+
+// Drinks routes
+index.get("/drinks", searchRateLimit.middleware, controller.getDrinks);
+index.post("/drinks", 
+    writeRateLimit.middleware, 
+    AuthMiddleware.apiKeyAuth, 
+    AuthorizationMiddleware.requireWritePermission,
+    validateCreateDrink, 
+    controller.createDrink,
+);
+index.patch("/drinks/:id", 
+    writeRateLimit.middleware, 
+    AuthMiddleware.apiKeyAuth, 
+    AuthorizationMiddleware.requireWritePermission,
+    validateCreateDrink, 
+    controller.updateDrink,
+);
+index.delete("/drinks/:id", 
+    writeRateLimit.middleware, 
+    AuthMiddleware.apiKeyAuth, 
+    AuthorizationMiddleware.requireDeletePermission,
+    controller.deleteDrink,
+);
